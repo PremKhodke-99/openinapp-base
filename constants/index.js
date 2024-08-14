199fadf870229e150ffe1ee300bd71cccc1ed711
Prem <premkhodke.99@gmail.com>
2024-08-14 11:55:47 +0530

    Constant data for mapping over

---
+++ constants/index.js
@@ -0,0 +1,30 @@
+export const sidebar = [
+    {
+        image: '/assets/sidebar/Category.png',
+        title: 'Dashboard'
+    },
+    {
+        image: '/assets/sidebar/Chart.png',
+        title: 'Upload'
+    },
+    {
+        image: '/assets/sidebar/Ticket.png',
+        title: 'Invoice'
+    },
+    {
+        image: '/assets/sidebar/Document.png',
+        title: 'Schedule'
+    },
+    {
+        image: '/assets/sidebar/Calendar.png',
+        title: 'Calendar'
+    },
+    {
+        image: '/assets/sidebar/Notification.png',
+        title: 'Notification'
+    },
+    {
+        image: '/assets/sidebar/Setting.png',
+        title: 'Setting'
+    },
+]
